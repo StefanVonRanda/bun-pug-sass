946d168559988892e6752db1d002b40d5582e2f4
StefanVonRanda <stefan@randa.me>
2024-11-12 14:35:38 +0100

    decreased noise when running dev

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const server = Bun.serve({
-	port: 5555,
+  port: 5555,
   async fetch(req) {
     const path = new URL(req.url).pathname;
-		if (path === "/") return new Response(Bun.file(`dist/index.html`));
-		else {return new Response(Bun.file(`dist${path}`))}
-	},
+    if (path === "/") return new Response(Bun.file(`dist/index.html`));
+    else { return new Response(Bun.file(`dist${path}`)); }
+  },
 });
 
 console.log(`Listening on ${server.url}`);
