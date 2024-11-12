e64b6b176dcf0fbd20ba9a9e0b7ee65b4f015c39
StefanVonRanda <stefan@randa.me>
2024-11-12 13:32:10 +0100

    added webserver

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+	port: 5555,
+  async fetch(req) {
+    const path = new URL(req.url).pathname;
+		if (path === "/") return new Response(Bun.file(`dist/index.html`));
+		else {return new Response(Bun.file(`dist${path}`))}
+	},
+});
+
+console.log(`Listening on ${server.url}`);
